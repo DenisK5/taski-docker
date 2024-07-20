e75bb6c31fd49694305535c43ed8f56f42469c13
DenisK5 <denis.kondrashov1@gmail.com>
2024-07-20 16:14:47 +0300

    1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'token')
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'taskihome.zapto.org', '84.252.136.149']
 
 
 # Application definition
